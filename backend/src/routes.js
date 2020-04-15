0ce02e9d1998a944d5e3970d2fcf447c2386f519
Rhuana Pires <42420374+rhuanapires@users.noreply.github.com>
2020-04-15 12:37:58 -0300

    added frontend made with reactJS

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const OngControler = require('./controllers/OngController')
-const IncidentController = require('./controllers/IncidentController')
-const ProfileController = require('./controllers/ProfileController')
-const SessionController = require('./controllers/SessionController')
+const express = require("express");
+const OngControler = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
 const router = express.Router();
 
-router.get('/sessions', SessionController.create)
+router.post("/sessions", SessionController.create);
 
-router.get('/ongs', OngControler.index);
-router.post('/ongs', OngControler.create);
+router.get("/ongs", OngControler.index);
+router.post("/ongs", OngControler.create);
 
-router.get('/profile', ProfileController.listEspecificForOng);
+router.get("/profile", ProfileController.listEspecificForOng);
 
-router.post('/incidents', IncidentController.create);
-router.get('/incidents', IncidentController.index);
-router.delete('/incidents/:id', IncidentController.delete);
+router.post("/incidents", IncidentController.create);
+router.get("/incidents", IncidentController.index);
+router.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = router;
